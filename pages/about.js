7aa5a4ffb617e9bdf5abf7aab8251b26e7ad685c
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-03-02 07:53:25 +0100

    Update about page; fetch data from Github account

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,37 @@
-import Layout from '../components/Layout'
+import fetch from 'isomorphic-unfetch'
 
-const About = () => (
-  <Layout title='About'>
-    <p>A Javascript programmer</p>
-    <img src='/static/javascript_logo.png' alt='Javascript Logo' />
-  </Layout>
-)
+import Layout from '../components/Layout'
+import Error from './_error'
+
+const About = ({ user, status }) => {
+  return (
+    <>
+      {status ? (
+        <Error message={status} />
+      ) : (
+        <Layout title='About'>
+          <p>A Javascript programmer</p>
+          <p>UserName: {user.login}</p>
+          <img src={user.avatar_url} alt='User Avatar' />
+        </Layout>
+      )}
+    </>
+  )
+}
+
+About.getInitialProps = async () => {
+  try {
+    const res = await fetch('https://api.github.com/users/TituxMetal')
+    const data = await res.json()
+    const status = res.status > 200 ? data.message : false
+
+    return {
+      user: data,
+      status
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 export default About
